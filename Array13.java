64f98c48b8eeab18da02cd41db273c04675b2876
Rithik Kumar <138803799+rithikkr@users.noreply.github.com>
2024-04-26 13:08:20 +0530

    Add files via upload

---
+++ Array13.java
@@ -0,0 +1,22 @@
+public class Array13 {
+
+    public static int buyAndSellStock(int prices[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length; i ++){
+            if (buyPrice < prices[i]) { //profit
+                int profit = prices[i] - buyPrice; //today's profit
+                maxProfit = Math.max(maxProfit, profit);
+            }else{
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+   public static void main(String[] args) {
+        int prices[] = {7, 1, 5, 3, 6, 4}; //Time complaxity - O(n)
+        System.out.println(buyAndSellStock(prices));
+   } 
+}
